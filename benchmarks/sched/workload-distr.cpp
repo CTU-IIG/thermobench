598eb0f5d460c3bfb32a994e2a50fe7d7ba34b9b
Michal Sojka <michal.sojka@cvut.cz>
2020-02-13 01:34:29 +0100

    benchmarks/workload-distr: Allow more CPU configurations and delays
    The command line parameters are:
    
    -m specifies CPU mask of all CPUs used during experiments
    -i specifies a mask of initially running CPUs
    -d specifies delay (in ms) between runs
    
    Examples:
    
    -m0xf -i0x3 runs CPUs: 01 23 01 23 ...
    -m0xf -i0x5 runs CPUs: 02 13 02 13 ...
    -m0x1b -i0x9 runs CPUs: 03 14 03 14 ...
    -m0x1ff -i0x3 runs CPUs: 01 23 40 12 34 01 23 ...

--- benchmarks/sched/workload-distr.cpp
+++ benchmarks/sched/workload-distr.cpp
@@ -7,32 +7,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <bsort.c>
+#include "bsort.c"
 #include <semaphore.h>
 #include <mutex>
 #include <condition_variable>
-
-class semaphore {
-public:
-    semaphore(int value) { sem_init(&sem, 0, value); };
-    ~semaphore() { sem_destroy(&sem); }
-    void wait() { sem_wait(&sem); }
-    void post() { sem_post(&sem); }
-private:
-    sem_t sem;
-};
+#include <iostream>
+#include <exception>
+#include <cstring>
+#include <chrono>
 
 using namespace std;
 
 int loops_per_print = 1;
+int delay_ms = 0;
 
-bool sequential = true;
-semaphore seq_sem(1);
+// C++ wrapper over C's cpu_set_t
+class cpu_set {
+public:
+    static constexpr int max_cpus = 64;
 
-uint64_t cpu_mask = 0xffffffffffffffff;
+    cpu_set() : s(CPU_ALLOC(max_cpus)) { zero(); }
+    cpu_set(const cpu_set& o) : s(CPU_ALLOC(max_cpus)) { memcpy(s, o.s, size()); }
+    cpu_set(cpu_set&& o) : s(o.s) { o.s = nullptr; }
+    cpu_set(long int mask) : cpu_set() {
+        for (size_t i = 0; i < sizeof(mask) * 8; i++)
+            if (mask & (1UL<<i))
+                set(i); }
+    ~cpu_set() { CPU_FREE(s); }
+
+    void zero() { CPU_ZERO_S(size(), s); }
+    bool is_set(unsigned i) const { return !!CPU_ISSET_S(i, size(), s); }
+    void set(unsigned i) { CPU_SET_S(i, size(), s); }
+    void clr(unsigned i) { CPU_CLR_S(i, size(), s); }
+    cpu_set& operator &=(const cpu_set& o) { CPU_AND_S(size(), s, s, o.s); return *this; }
+    cpu_set& operator ^=(const cpu_set& o) { CPU_XOR_S(size(), s, s, o.s); return *this; }
+    cpu_set& operator |=(const cpu_set& o) { CPU_OR_S(size(), s, s, o.s); return *this; }
+    bool operator ==(cpu_set& o) { return CPU_EQUAL_S(size(), s, o.s); }
+    bool operator !=(cpu_set& o) { return !(o == *this); }
+    unsigned count() const { return CPU_COUNT_S(size(), s); }
+    size_t size() const { return CPU_ALLOC_SIZE(max_cpus); }
+    cpu_set_t* ptr() const { return s; }
+    int as_int() const { int mask = 0; for (int i = 0; i < max_cpus; i++) if (is_set(i)) mask |= 1<<i; return mask; }
+    explicit operator bool() const { return count() > 0; }
+
+    cpu_set &operator =(const cpu_set& o) { memcpy(s, o.s, size()); return *this; }
+    cpu_set operator &(const cpu_set& o) const { return cpu_set(*this) &= o; }
+    cpu_set operator |(const cpu_set& o) const { return cpu_set(*this) |= o; }
+    cpu_set operator ^(const cpu_set& o) const { return cpu_set(*this) ^= o; }
+
+    unsigned highest() const {
+        for (int i = max_cpus - 1; i >= 0; i--)
+            if (is_set(i))
+                return i;
+        throw runtime_error("empty cpu_set");
+    }
+    unsigned lowest() const {
+        for (int i = 0; i < max_cpus; i++)
+            if (is_set(i))
+                return i;
+        throw runtime_error("empty cpu_set");
+    }
+    unsigned next_set(int j) {
+        for (int i = j + 1; i != j; i = (i + 1) % max_cpus)
+            if (is_set(i))
+                return i;
+        throw runtime_error("empty cpu_set");
+    }
+private:
+    cpu_set_t *s;
+};
+
+cpu_set cpu_mask;
 
 struct synchronizer {
-    uint64_t cpu_turn_mask = 1;
+    cpu_set run, done;
+    unsigned num_cpus = 0, num_done = 0;
+
     unsigned work_done = 0;
     mutex m;
     condition_variable cv;
@@ -49,35 +99,55 @@ int bench_func()
 
 void *benchmark_loop(void *ptr)
 {
-    int thread_id = (intptr_t)ptr;
+    int cpu = (intptr_t)ptr;
 
     while (1) {
-        if (sequential) {
+        {
             unique_lock<mutex> lock(s.m);
-            while (((1 << thread_id) & s.cpu_turn_mask) == 0)
+            while (!s.run.is_set(cpu))
                 s.cv.wait(lock);
+//            printf("running at CPU%u\n", cpu);
+//            fflush(stdout);
         }
 
-        printf("running at CPU%u\n", thread_id);
-        fflush(stdout);
-        int wd;
-        for (int j = 0; j < loops_per_print || loops_per_print < 1; ++j)
-            wd = bench_func();
+        using namespace chrono;
 
-        if (sequential) {
-            int msb = 31 - __builtin_clz(cpu_mask);
-            int lsb = __builtin_ctz(cpu_mask);
-            //int bits = __builtin_popcount(cpu_mask);
+
+        auto start = steady_clock::now();
+        int wd = 0;
+        for (int j = 0; j < loops_per_print || loops_per_print < 1; ++j)
+            wd += bench_func();
+        auto stop = steady_clock::now();
+
+        {
             unique_lock<mutex> lock(s.m);
+            s.run.clr(cpu);
+            s.done.set(cpu);
             s.work_done += wd;
-            printf("work_done=%u\n", s.work_done);
-            fflush(stdout);
-            do {
-                s.cpu_turn_mask <<= 1;
-                if (31 - __builtin_clz(s.cpu_turn_mask) > msb)
-                    s.cpu_turn_mask = (1 << lsb);
-            } while ((s.cpu_turn_mask & cpu_mask) == 0);
-            s.cv.notify_all();
+            cout << "work_done=" << s.work_done << endl;
+            cout << "duration=" << duration_cast<milliseconds>(stop - start).count() << endl;
+            cout << flush;
+
+            if (++s.num_done == s.num_cpus) {
+                usleep(delay_ms * 1000);
+                cpu_set next;
+                cout << "-- Next CPUs:";
+                for (int i = 0; i < next.max_cpus; i++) {
+                    if (s.done.is_set(i)) {
+                        int next_cpu = i;
+                        while (s.done.is_set(next_cpu) || next.is_set(next_cpu))
+                            next_cpu = cpu_mask.next_set(next_cpu);
+                        cout << " " << next_cpu;
+                        next.set(next_cpu);
+                    }
+                }
+                cout << endl << flush;
+
+                s.num_done = 0;
+                s.run = next;
+                s.done.zero();
+                s.cv.notify_all();
+            }
         }
     }
     return NULL;
@@ -110,17 +180,34 @@ long int xstrtol(const char *str, const char *err_msg)
 
 int main(int argc, char *argv[])
 {
-    const int num_proc = sysconf(_SC_NPROCESSORS_ONLN);
     int opt;
 
-    while ((opt = getopt(argc, argv, "l:m:")) != -1) {
+    sched_getaffinity(0, cpu_mask.size(), cpu_mask.ptr());
+
+    while ((opt = getopt(argc, argv, "d:l:m:i:")) != -1) {
         switch (opt) {
+        case 'd':
+            delay_ms = xstrtol(optarg, "-d");
+            break;
         case 'l':
             loops_per_print = xstrtol(optarg, "-l");
             break;
-        case 'm':
-            cpu_mask = xstrtol(optarg, "-m");
+        case 'm': {
+            cpu_set m(xstrtol(optarg, "-m"));
+            if (!m)
+                errx(1, "Mask cannot be 0");
+            if (m ^ (m & cpu_mask))
+                errx(1, "Invalid CPU(s) in mask; given: %#x, allowed: %#x", m.as_int(), cpu_mask.as_int());
+            cpu_mask &= m;
             break;
+        }
+        case 'i': {
+            cpu_set m(xstrtol(optarg, "-i"));
+            if (!m)
+                errx(1, "Initial mask cannot be 0");
+            s.run = m;
+            break;
+        }
         default: /* '?' */
             fprintf(stderr, "Usage: %s [-l loops ] [-m cpu_mask]\n",
                     argv[0]);
@@ -128,18 +215,28 @@ int main(int argc, char *argv[])
         }
     }
 
-    for (int i = 0; i < num_proc; i++) {
-        pthread_attr_t attr;
-        cpu_set_t cpuset;
-        pthread_t tid;
+    if (!s.run)
+        s.run.set(cpu_mask.lowest());
 
-        if ((cpu_mask & (1U << i)) == 0)
+    if ((cpu_mask & s.run) != s.run)
+        errx(1, "Invalid initial mask");
+    s.num_cpus = s.run.count();
+
+    if (cpu_mask.count() - s.num_cpus < s.num_cpus)
+        errx(1, "Initial CPU count (%d) must be less than half of total CPU count (%d/2)",
+             s.num_cpus, cpu_mask.count());
+
+
+    for (int i = 0; i < cpu_mask.max_cpus; i++) {
+        if (!cpu_mask.is_set(i))
             continue;
 
-        CPU_ZERO(&cpuset);
-        CPU_SET(i, &cpuset);
+        pthread_attr_t attr;
+        cpu_set cpuset(1 << i);
+        pthread_t tid;
+
         pthread_attr_init(&attr);
-        pthread_attr_setaffinity_np(&attr, sizeof(cpuset), &cpuset);
+        pthread_attr_setaffinity_np(&attr, cpuset.size(), cpuset.ptr());
         pthread_create(&tid, &attr, benchmark_loop, (void *)(intptr_t)i);
     }
 
