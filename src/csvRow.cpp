0aee890312960d341783a758e2b3db15c62ed843
barinalex <barinale@fel.cvut.cz>
2020-03-13 21:55:26 +0100

    introduce a class CsvColumn, container CsvColumns and a class csvRow which can be used for writing rows to CSV file

---
+++ src/csvRow.cpp
@@ -0,0 +1,114 @@
+#include "csvRow.h"
+
+/* CsvColumn implementation */
+
+CsvColumn::CsvColumn(std::string name, unsigned int order){
+    this->name = name;
+    this->order = order;
+}
+
+std::string CsvColumn::getName(){
+    return this->name;
+}
+
+unsigned int CsvColumn::getOrder(){
+    return this->order;
+}
+
+
+/* CsvColumns implementation */
+
+CsvColumns::CsvColumns(){
+    this->size = 0;
+};
+
+CsvColumns::~CsvColumns(){
+    while(!this->columns.empty()){
+        delete this->columns.back();
+        this->columns.pop_back();
+    }
+};
+
+CsvColumn* CsvColumns::add(std::string name){
+    CsvColumn* newColumn = new CsvColumn(name, size);
+    if(newColumn){
+        ++size;
+        columns.push_back(newColumn);
+        return columns.back();
+    }
+    return NULL;
+}
+
+unsigned int CsvColumns::getSize(){
+   return this->size; 
+}
+
+
+CsvColumn* CsvColumns::getColumn(unsigned int index){
+    if(index < size){
+        return columns[index];
+    }
+    return NULL;
+}
+
+
+/* CsvRow implementation */
+
+void CsvRow::set(CsvColumn* column, std::string data){
+    const unsigned int order = column->getOrder();
+    data = makeCsvSafe(data);
+    data.push_back(',');
+    if(order < this->row.size()){
+        row[order] = data;
+    }
+    else{
+        while(row.size() < order){
+            row.push_back(",");
+        }
+        row.push_back(data);
+    }
+};
+
+std::string CsvRow::getRow(){
+    std::string line;
+    for(std::string str: row){
+        line.append(str);
+    }
+    line.pop_back();
+    line.push_back('\n');
+    return line;
+}
+
+void CsvRow::write(FILE *fp){
+   fprintf(fp, "%s", (this->getRow()).c_str());
+}
+
+void CsvRow::clean(){
+    while(!row.empty()) 
+        row.pop_back();
+}
+
+
+std::string makeCsvSafe(std::string unsafe){
+    //Each of the embedded double-quote characters 
+    //must be represented by a pair of double-quote characters
+    int first = 0, last = unsafe.size();
+    //find pairs of double-quote and quote them
+    while((first = unsafe.find_first_of('"', first)) >= 0 && 
+            (last = unsafe.find_last_of('"', last)) >= 0 &&
+            first < last){
+        unsafe.insert(first, "\"");
+        unsafe.insert(last + 1, "\"");
+        first+=2; //after insert index of founded character is incremented, therefore +2
+        last--;
+    }
+    //Fields with embedded commas or line breaks characters must be quoted
+    int comma, whitespace; 
+    if((comma = unsafe.find_first_of(',')) >= 0 || 
+            (whitespace = unsafe.find_first_of(' ')) >= 0){
+        unsafe.insert(0, "\"");
+        unsafe.push_back('"');
+    }
+    return unsafe;
+}
+
---
+++ src/csvRow.h
@@ -0,0 +1,53 @@
+#ifndef CSVROW
+#define CSVROW
+
+#include <iostream>
+#include <vector>
+
+std::string makeCsvSafe(std::string unsafe);
+
+class CsvColumn{
+    private:
+        std::string name;
+        unsigned int order;
+
+    public:
+        CsvColumn(std::string name, unsigned int order);
+
+        std::string getName();
+
+        unsigned int getOrder();
+};
+
+class CsvColumns{
+    private:
+        std::vector<CsvColumn*> columns;
+        unsigned int size;
+    
+    public:
+        CsvColumns();
+
+        ~CsvColumns();
+
+        CsvColumn* add(std::string name);
+        
+        unsigned int getSize();
+
+        CsvColumn* getColumn(unsigned int index);
+};
+
+class CsvRow{
+    private:
+        std::vector<std::string> row;
+
+    public:
+        void set(CsvColumn* column, std::string data);
+
+        std::string getRow();
+
+        void write(FILE *fp);
+
+        void clean();
+};
+
+#endif
