098ab55caabdcb9053a8df12f5b4afe566d346d0
Michal Sojka <michal.sojka@cvut.cz>
2020-05-31 23:22:03 +0200

    julia: Update docs
    - Do not use docs/ project, but the main one. This is my attempt to
      run jldoctests with the same environment as tests.
    - Copy&paste GitHub Action workflow from
      https://juliadocs.github.io/Documenter.jl/stable/man/hosting/#GitHub-Actions-1.
      The content will be updated in next commits.

--- julia/docs/make.jl
+++ julia/docs/make.jl
@@ -1,23 +1,26 @@
 using Documenter
 using Thermobench
 
+DocMeta.setdocmeta!(Thermobench,
+                    :DocTestSetup, :(using Thermobench, DataFrames; cd(joinpath(dirname(pathof(Thermobench)), "..", "test")));
+                    recursive=true)
 makedocs(
     sitename = "Thermobench",
     format = Documenter.HTML(),
     modules = [Thermobench]
 )
 
-using DocumenterMarkdown
-makedocs(
-    sitename = "Thermobench",
-    format = DocumenterMarkdown.Markdown(),
-    build = "build-markdown",
-    modules = [Thermobench]
-)
+# using DocumenterMarkdown
+# makedocs(
+#     sitename = "Thermobench",
+#     format = DocumenterMarkdown.Markdown(),
+#     build = "build-markdown",
+#     modules = [Thermobench]
+# )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/CTU-IIG/thermobench.git"
+)
