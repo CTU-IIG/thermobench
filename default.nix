c89adad19830a3f8ab3c2a9772a660d963a5483e
Michal Sojka <michal.sojka@cvut.cz>
2020-11-02 11:45:47 +0100

    nix: Add boost dependency

--- default.nix
+++ default.nix
@@ -15,6 +15,13 @@ stdenv.mkDerivation {
     ## until next release, we live without the benchmarks.
     # submodules = true;
   };
+  mesonFlags = "-Dauto_features=enabled";
+
+  # Meson is no longer able to pick up Boost automatically.
+  # https://github.com/NixOS/nixpkgs/issues/86131
+  BOOST_INCLUDEDIR = "${stdenv.lib.getDev boost}/include";
+  BOOST_LIBRARYDIR = "${stdenv.lib.getLib boost}/lib";
+
   nativeBuildInputs = [ meson ninja pkg-config ];
-  buildInputs = [ libev opencl-headers ocl-icd ];
+  buildInputs = [ libev opencl-headers ocl-icd boost ];
 }
