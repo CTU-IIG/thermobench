acbed76ac4391c5a41ac0cc627363b52844f2584
Michal Sojka <michal.sojka@cvut.cz>
2020-11-21 10:50:44 +0100

    clang-format util.hpp

--- src/util.hpp
+++ src/util.hpp
@@ -7,24 +7,27 @@
 // convert value initialization into default initialization.
 //
 // Source: https://stackoverflow.com/a/21028912/902448
-template <typename T, typename A=std::allocator<T>>
-class default_init_allocator : public A
-{
+template <typename T, typename A = std::allocator<T>>
+class default_init_allocator : public A {
     typedef std::allocator_traits<A> a_t;
+
 public:
-    template <typename U> struct rebind {
+    template <typename U>
+    struct rebind {
         using other = default_init_allocator<U, typename a_t::template rebind_alloc<U>>;
     };
 
     using A::A;
 
     template <typename U>
-    void construct(U* ptr) noexcept(std::is_nothrow_default_constructible<U>::value) {
-        ::new(static_cast<void*>(ptr)) U;
+    void construct(U *ptr) noexcept(std::is_nothrow_default_constructible<U>::value)
+    {
+        ::new (static_cast<void *>(ptr)) U;
     }
-    template <typename U, typename...Args>
-    void construct(U* ptr, Args&&... args) {
-        a_t::construct(static_cast<A&>(*this), ptr, std::forward<Args>(args)...);
+    template <typename U, typename... Args>
+    void construct(U *ptr, Args &&... args)
+    {
+        a_t::construct(static_cast<A &>(*this), ptr, std::forward<Args>(args)...);
     }
 };
 
